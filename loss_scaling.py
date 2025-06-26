14afa8614fefcb576f7fb9d03239a760e7513f1a
Wesley B <tie.yob@gmail.com>
2025-06-25 21:32:46 -0700

    update v3.

--- loss_scaling.py
+++ loss_scaling.py
@@ -10,9 +10,10 @@ def scaled_loss(model, loss_function, batch, loss_modifier):
 
 path = './model'
 device = 'cuda'
-batch = torch.tensor([[1,2,3], [4,5,6]])
-loss_modifier = torch.tensor([[0.05,0.1,0.5], [1.0,0.1,0.05]])
+batches = torch.tensor([[[1,2,3], [4,5,6]], [[7,8,9], [10,11,12]]])
+loss_modifier = torch.tensor([[[0.05,0.1,0.5], [1.0,0.1,0.05]], [[0.05,0.1,0.5], [1.0,0.1,0.05]]])
 loss_function = torch.nn.CrossEntropyLoss(reduction='none')
 model = AutoModelForCausalLM.from_pretrained(path, torch_dtype=torch.bfloat16, attn_implementation="flash_attention_2").to(device)
-scaled_loss(model,loss_function,batch,loss_modifier)
+for batch in batches:
+    scaled_loss(model,loss_function,batch,loss_modifier)
 
